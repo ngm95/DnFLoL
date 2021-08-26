737a918a4daebd2f9e647e4a9930a81380c87e9c
ngm95 <51411433+ngm95@users.noreply.github.com>
2021-08-26 18:05:21 +0900

    Vue.js 연동
    연동이랄것도없이 내부에서 빌드하고 빌드 결과물을 사용하는 것임. Vue.js는 VS Code를 이용해 개발 예정

--- dnflol/src/main/java/com/project/dnflol/Controller/TestController.java
+++ dnflol/src/main/java/com/project/dnflol/Controller/TestController.java
@@ -2,11 +2,18 @@ package com.project.dnflol.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class TestController {
 	@RequestMapping("test")
 	public String test() {
 		return "테스트 = test";
+	}
+	@RequestMapping("/")
+	public ModelAndView vue() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		return mv;
 	}
 }
