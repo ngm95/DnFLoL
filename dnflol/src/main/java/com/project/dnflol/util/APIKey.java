b3c52552ee26e99c8eb4f71f6e78bb0669e14343
ngm95 <51411433+ngm95@users.noreply.github.com>
2021-11-03 00:23:47 +0900

    LOL API Key Update

--- dnflol/src/main/java/com/project/dnflol/util/APIKey.java
+++ dnflol/src/main/java/com/project/dnflol/util/APIKey.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class APIKey {
-	private String LOL_API_KEY = "RGAPI-ca7dbf95-314e-43be-b2e4-81c123f0f996";
+	private String LOL_API_KEY = "RGAPI-4dddd0f5-3944-4c40-96dc-9a8d90d5927c";
 	private String DNF_API_KEY = "VdItQKfKUZWekekAag0O6i7vxRhE9TMs";
 }
