31172546f874f7a5bee3f01fed50a2be081a280c
ngm95 <51411433+ngm95@users.noreply.github.com>
2021-11-16 19:24:39 +0900

    Update APIKey.java

--- dnflol/src/main/java/com/project/dnflol/util/APIKey.java
+++ dnflol/src/main/java/com/project/dnflol/util/APIKey.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class APIKey {
-	private String LOL_API_KEY = "RGAPI-2c5f60ac-91db-4834-85bc-8f77df1f6d08";
+	private String LOL_API_KEY = "RGAPI-1ed45782-04e3-4738-a3d1-d053988289fe";
 	private String DNF_API_KEY = "VdItQKfKUZWekekAag0O6i7vxRhE9TMs";
 }
